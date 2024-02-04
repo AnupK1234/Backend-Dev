89011fb1e58c34794e1bc69670eece4123fb5d22
Anup Khismatrao <khismatrao2003@gmail.com>
2024-02-04 13:07:38 +0530

    Add watchHistory and updated user router

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
-import { ApiError } from "../utils/ApiError.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 // here we dont require `res` so we kept it blank usign `_`
 export const verifyJWT = asyncHandler(async (req, _, next) => {
